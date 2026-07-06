0839a7e0426f631093520c03caf248a410c2c315
KARTHIK.R <155284320+karthivisva@users.noreply.github.com>
2026-07-06 20:34:32 +0530

    Fix WelcomeOverlay import

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://suba-backend-cl2r.onrender.com',
+  baseURL: 'https://suba-backend-cl2r.onrender.com/api/v1',
 })
 
 api.interceptors.request.use((config) => {
